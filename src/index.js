1c234d37f080d080b4977471324c297020653696
Dezolder <evgeniy.zhukov.2012@gmail.com>
2023-02-10 15:09:07 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import data from "./data"
+import './index.scss'
+// import css from "file.css";
+const root = document.querySelector('#app')
+
+function renderItem(item) {
+  const li = document.createElement('li')
+  li.textContent = item.title
+  console.log(item.title)
+  root.append(li)
+}
+
+data.forEach(renderItem)
