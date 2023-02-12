beca6e4ba541219c3447961548a58bafef2f1604
Dezolder <evgeniy.zhukov.2012@gmail.com>
2023-02-12 02:22:18 +0200

    eslint formatter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,42 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-    extends: ["eslint:recommended", "plugin:react/recommended"],
-    overrides: [],
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     },
-    plugins: ["react"],
-    rules: {},
-};
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    },
+    "settings": {
+        "react": {
+            "version": 'detect'
+        }
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import data from "./data"
+import data from './data'
 import './index.scss'
 // import css from "file.css";
 const root = document.querySelector('#app')
 
 function renderItem(item) {
-  const li = document.createElement('li')
-  li.textContent = item.title
-  console.log(item.title)
-  root.append(li)
+    const li = document.createElement('li')
+    li.textContent = item.title
+    console.log(item.title)
+    console.log('')
+
+    root.append(li)
 }
 
 data.forEach(renderItem)
